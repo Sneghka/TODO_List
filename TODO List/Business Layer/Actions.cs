c8aabf63f2e4c12862982bbb90b278e6f9624309
sneghka <sneghkan@gmail.com>
2019-05-18 17:13:59 +0300

    business layer class

---
+++ TODO List/Business Layer/Actions.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Business_Layer
+{
+    public class Actions
+    {
+        private static string path = @"D:\Sneghka\IT\Visual Studio\TODO List\Tasks.json";
+        //private static string DatabaseFilename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "database", "Tasks.json");
+
+        public static List<T> LoadData<T>()
+        {    
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                return JsonConvert.DeserializeObject<List<T>>(json);
+            }
+        }
+
+        public static void SaveData<T>(T tasks)
+        {
+            var newJsonFile = JsonConvert.SerializeObject(tasks, Formatting.Indented);
+            File.WriteAllText(path, newJsonFile);
+        }      
+    }
+}
